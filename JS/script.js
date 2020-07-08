66ca81988b7812ffa23e5ba69da75235ff39309d
shekar88 <47589434+shekar88@users.noreply.github.com>
2020-07-08 16:52:11 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -64,15 +64,11 @@ function Ordinal(n) {
     for(var i = 0; i < keys.length; i++){
         obj[keys[i]] = a[i]+ ','+ myBgColor[i];
 	}
-	console.log('obj');
-	console.log(obj);
 
     // Printing object
     for (var key of Object.keys(obj)) {
         newObjArr[key] = obj[key].split(',');
 	}
-	console.log('new Obj ');
-	console.log(newObjArr);
 
     $('#target-area-here').html(`
 		<div class="row">
